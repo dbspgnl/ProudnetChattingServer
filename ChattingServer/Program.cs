ad24e8169a64686eafc83a4b45187def7af5d14d
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2022-01-18 11:02:10 +0900

    + 최초 설정 / 시스템 안내 / 채팅 구현 + common / server / client

---
+++ ChattingServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Nettention.Proud;
+using ChattingCommon;
+
+namespace ChattingServer
+{
+    namespace ChattingServer
+    {
+
+        class Program
+        {
+            static void Main()
+            {
+                ServerLauncher server = new ServerLauncher();
+
+                try
+                {
+                    server.ServerStart();
+                    Console.Write("Server started\n");
+
+                    while (server.RunLoop)
+                    {
+                        if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape && Console.ReadKey(true).Key == ConsoleKey.Delete)
+                        {
+                            break;
+                        }
+                        System.Threading.Thread.Sleep(1000);
+                    }
+
+                    Console.Write("Server Closed\n");
+                    server.Dispose();
+
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ ChattingServer/process/Process.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nettention.Proud;
+
+namespace ChattingServer.process
+{
+    internal class CommonProcess
+    {
+        static S2C.Proxy S2CProxy = new S2C.Proxy();
+        static C2S.Stub C2SStub = new C2S.Stub();
+
+        public void InitStub()
+        {
+            // Stub에 등록
+            C2SStub.Chat = Chat;
+
+            ServerLauncher.NetServer.AttachProxy(S2CProxy);
+            ServerLauncher.NetServer.AttachStub(C2SStub);
+        }
+        // Chat 함수 로직 작성
+        static public bool Chat(HostID remote, RmiContext rmiContext, string str)
+        {
+            Console.WriteLine(str);
+            S2CProxy.NotifyChat(ServerLauncher.NetServer.GetClientHostIDs(), rmiContext, str);
+            return true;
+        }
+        public void SystemChat(string str)
+        {
+            S2CProxy.SystemChat(ServerLauncher.NetServer.GetClientHostIDs(), RmiContext.ReliableSend, str);
+        }
+
+    }
+}
